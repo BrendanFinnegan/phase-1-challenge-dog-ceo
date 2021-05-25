4ed0a93cde2c116fb26514909283a323644a8370
Ryan Hutzley <ryanhutzley@gmail.com>
2021-05-25 16:22:41 -0400

    challenge 3

--- src/index.js
+++ src/index.js
@@ -1,23 +1,27 @@
-console.log('%c HI', 'color: firebrick')
-document.addEventListener('DOMContentLoaded', ()=> {
-fetch('https://dog.ceo/api/breeds/image/random/4').then( res => res.json()).then( data => data.message.forEach((x) => {
-   let img = document.createElement('img') 
-   img.src = x
-document.querySelector('#dog-image-container').append(img);
-fetch('https://dog.ceo/api/breeds/list/all').then(res => res.json()).then(data => {
-    const breedList = data.message
-    let breedArray = Object.entries(breedList)
-    breedArray.forEach((param)=> {
-        let element = document.createElement('li')
-element.textContent = param
-        document.querySelector('#dog-breeds').append(element)
-    })
-    
-})
+console.log("%c HI", "color: firebrick");
+document.addEventListener("DOMContentLoaded", () => {
+  fetch("https://dog.ceo/api/breeds/image/random/4")
+    .then((res) => res.json())
+    .then((data) =>
+      data.message.forEach((x) => {
+        let img = document.createElement("img");
+        img.src = x;
+        document.querySelector("#dog-image-container").append(img);
+      })
+    );
+
+  fetch("https://dog.ceo/api/breeds/list/all")
+    .then((res) => res.json())
+    .then((data) => {
+      const breedList = data.message;
+      let breedArray = Object.entries(breedList);
+      breedArray.forEach((param) => {
+        let element = document.createElement("li");
+        element.textContent = param;
+        element.addEventListener('click', e => e.target.style.color = 'red')
+        document.querySelector("#dog-breeds").append(element);
+      });
+    });
 
 
-}))
-
-
-}) 
-
+});
