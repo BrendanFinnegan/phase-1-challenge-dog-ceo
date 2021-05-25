7c71cd83354341ef75a48dad8459822c1e79d113
BrendanFinnegan <bf289907@gmail.com>
2021-05-25 14:45:19 -0400

    challenge 1

--- src/index.js
+++ src/index.js
@@ -1,1 +1,14 @@
 console.log('%c HI', 'color: firebrick')
+document.addEventListener('DOMContentLoaded', ()=> {
+fetch('https://dog.ceo/api/breeds/image/random/4').then( res => res.json()).then( data => data.message.forEach((x) => {
+   let img = document.createElement('img') 
+   img.src = x
+document.querySelector('#dog-image-container').append(img);
+fetch('https://dog.ceo/api/breeds/list/all').then(res => res.json()).then(data => ()=>{})
+
+
+}))
+
+
+}) 
+
