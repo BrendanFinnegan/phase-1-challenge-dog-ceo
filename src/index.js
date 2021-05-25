4c449229bd55edc25a425965fd446b6dfa486c5f
BrendanFinnegan <bf289907@gmail.com>
2021-05-25 15:45:00 -0400

    second challenge

--- src/index.js
+++ src/index.js
@@ -4,7 +4,16 @@ fetch('https://dog.ceo/api/breeds/image/random/4').then( res => res.json()).then
    let img = document.createElement('img') 
    img.src = x
 document.querySelector('#dog-image-container').append(img);
-fetch('https://dog.ceo/api/breeds/list/all').then(res => res.json()).then(data => ()=>{})
+fetch('https://dog.ceo/api/breeds/list/all').then(res => res.json()).then(data => {
+    const breedList = data.message
+    let breedArray = Object.entries(breedList)
+    breedArray.forEach((param)=> {
+        let element = document.createElement('li')
+element.textContent = param
+        document.querySelector('#dog-breeds').append(element)
+    })
+    
+})
 
 
 }))
