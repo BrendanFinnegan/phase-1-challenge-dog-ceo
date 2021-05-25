34236934de3f206d6225bdc2e1784115613db985
Ryan Hutzley <ryanhutzley@gmail.com>
2021-05-25 17:30:40 -0400

    challenge 4

--- src/index.js
+++ src/index.js
@@ -9,6 +9,8 @@ document.addEventListener("DOMContentLoaded", () => {
         document.querySelector("#dog-image-container").append(img);
       })
     );
+  
+  let storageArray = []  
 
   fetch("https://dog.ceo/api/breeds/list/all")
     .then((res) => res.json())
@@ -18,10 +20,27 @@ document.addEventListener("DOMContentLoaded", () => {
       breedArray.forEach((param) => {
         let element = document.createElement("li");
         element.textContent = param;
-        element.addEventListener('click', e => e.target.style.color = 'red')
+        storageArray.push(element)
+        element.addEventListener(
+          "click",
+          (e) => (e.target.style.color = "red")
+        );
         document.querySelector("#dog-breeds").append(element);
       });
+      let listOfNodes = Array.from(document.querySelectorAll('li'))
+      let dropdown = document.querySelector("#breed-dropdown");
+      
+      dropdown.onchange = selection => {
+          selection = dropdown.value
+          let filteredArray = listOfNodes.filter(element => {
+              return element.textContent[0] === selection
+          })
+          document.querySelector('ul').innerHTML = ''
+          filteredArray.forEach(element => {
+              let li = document.createElement('li')
+              li.textContent = element.textContent
+              document.querySelector('ul').append(li)
+          })  
+      }
     });
-
-
-});
+})
